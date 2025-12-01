b09bc63a8b0f054e02aee2bbf979a24da6cd3a76
Abhinav M <130998983+abhinav0700@users.noreply.github.com>
2025-12-01 19:04:58 +0530

    Refactor App component with useState for weather
    Refactor imports and update weather state initialization.

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import WeatherDisplay from "./WeatherDisplay";
+import "./styles/App.css";
 
 const App = () => {
+
+  // hardcoded weather data stored using useState
+  const [weather, setWeather] = useState({
+    temperature: 25,
+    conditions: "Sunny"
+  });
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove main div */}
+      <WeatherDisplay weather={weather} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
